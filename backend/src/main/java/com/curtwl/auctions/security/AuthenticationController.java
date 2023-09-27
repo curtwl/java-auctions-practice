48554e1a9f67d3549d8fbfc17e96c7294bd34d36
Curt <121463645+curtwl@users.noreply.github.com>
2023-09-27 16:45:32 -0500

    Fix route mapping

--- backend/src/main/java/com/curtwl/auctions/security/AuthenticationController.java
+++ backend/src/main/java/com/curtwl/auctions/security/AuthenticationController.java
@@ -4,25 +4,40 @@ import com.curtwl.auctions.model.User;
 import com.curtwl.auctions.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/signup")
+@RequestMapping("/api")
 public class AuthenticationController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
-    @PostMapping("/api/signup")
+//    @PostMapping("/login")
+//    public String login(@RequestBody User user) {
+//        return "login";
+//    }
+
+    @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody User user) {
         System.out.println(user);
+        user.setUsername(user.getUsername());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
-        return ResponseEntity.ok("User registered successfully");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "User registered successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
