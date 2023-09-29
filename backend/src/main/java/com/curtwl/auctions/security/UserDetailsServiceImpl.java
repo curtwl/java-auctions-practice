35f51ca8ed815c35694998f336becbebcc63bfba
Curt <121463645+curtwl@users.noreply.github.com>
2023-09-29 18:47:43 -0500

    Replace InMemoryUserDetailsManager with DB

--- backend/src/main/java/com/curtwl/auctions/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/curtwl/auctions/security/UserDetailsServiceImpl.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,23 +16,8 @@ import java.util.ArrayList;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
-
-    public UserDetailsServiceImpl() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("pw"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("pw"))
-                .roles("ADMIN")
-                .build();
-        this.inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user, admin);
     }
 
     @Autowired
@@ -42,15 +25,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User " + username + " not found");
-//        }
-//        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
-//        return User.withUsername("admin")
-//                .password(encodedPassword)
-//                .roles("USER")
-//                .build();
-        return inMemoryUserDetailsManager.loadUserByUsername(username);
+        com.curtwl.auctions.model.User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + username + " not found");
+        }
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+//                .roles(user.getRoles())
+                .build();
+
     }
 }
