866d89b2be21654a3f3b9a509f3068d2ac69e313
Curt <121463645+curtwl@users.noreply.github.com>
2023-09-29 19:26:02 -0500

    Implement login and signup routes

--- backend/src/main/java/com/curtwl/auctions/security/AuthenticationController.java
+++ backend/src/main/java/com/curtwl/auctions/security/AuthenticationController.java
@@ -2,16 +2,27 @@ package com.curtwl.auctions.security;
 
 import com.curtwl.auctions.model.User;
 import com.curtwl.auctions.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import static io.jsonwebtoken.security.Keys.secretKeyFor;
 
 @RestController
 @CrossOrigin
@@ -20,14 +31,33 @@ public class AuthenticationController {
 
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private DaoAuthenticationProvider authenticationProvider;
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
-//    @PostMapping("/login")
-//    public String login(@RequestBody User user) {
-//        return "login";
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        System.out.println(user);
+        try {
+            Authentication authentication = authenticationProvider.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String token = Jwts.builder()
+                    .setSubject(user.getUsername())
+                    .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
+                    .signWith(secretKeyFor(SignatureAlgorithm.HS512))
+                    .compact();
+
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody User user) {
--- backend/src/main/java/com/curtwl/auctions/security/SecurityConfig.java
+++ backend/src/main/java/com/curtwl/auctions/security/SecurityConfig.java
@@ -3,9 +3,13 @@ package com.curtwl.auctions.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,18 +23,42 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends AbstractHttpConfigurer<SecurityConfig, HttpSecurity> {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/signup", "/login", "/api/auctions").permitAll()
+                        .requestMatchers("/api/signup", "/api/auctions", "/api/login").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .authenticationProvider(daoAuthenticationProvider());
 
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 
     @Bean
